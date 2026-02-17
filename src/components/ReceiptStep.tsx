afd1f5a0f67e4e95ee3236edece1eaa47c5572c5
Tibor Sohonyai <185492254+Rocka06@users.noreply.github.com>
2026-02-17 08:35:57 +0100

    added receipt step

--- src/components/ReceiptStep.tsx
+++ src/components/ReceiptStep.tsx
@@ -1,3 +1,66 @@
+import type { ChangeEvent } from "react";
+import { useOrder } from "../context/OrderContext";
+
 export default function ReceiptStep() {
-    return <p>Receipt</p>
+    const { order, setOrder } = useOrder();
+
+    if (!order.orderer) {
+        setOrder({
+            ...order,
+            orderer: { email: "", mobile: "", name: "" }
+        })
+    }
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+
+        if (order.orderer)
+            setOrder({
+                ...order,
+                orderer: { ...order.orderer, [name]: value }
+            });
+    };
+
+    return (
+        <div className="space-y-4">
+            <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Név</span>
+                </label>
+                <input
+                    type="text"
+                    name="name"
+                    value={order.orderer?.name}
+                    onChange={handleChange}
+                    className="input input-bordered w-full"
+                />
+            </div>
+
+            <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Email</span>
+                </label>
+                <input
+                    type="email"
+                    name="email"
+                    value={order.orderer?.email}
+                    onChange={handleChange}
+                    className="input input-bordered w-full"
+                />
+            </div>
+
+            <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Telefonszám</span>
+                </label>
+                <input
+                    type="text"
+                    name="mobile"
+                    value={order.orderer?.mobile}
+                    onChange={handleChange}
+                    className="input input-bordered w-full"
+                />
+            </div>
+        </div>
+    );
 }
